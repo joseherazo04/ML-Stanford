8f17e7091bcc7103ae7f57ce4269ad46cda8c0cb
José Herazo <joseherazo04@hotmail.com>
2020-07-07 15:58:43 -0500

    making some plots to understand this problem

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -11,19 +11,22 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
-
-
-
-
-
-
-
-
-
 % =========================================================================
 
+% Find Indices of Positive and Negative Examples
+pos = find(y == 1); 
+neg = find(y == 0);
 
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 hold off;
+
+% If i'd need to visualize how every feature affects the label i'd run
+%feature X1
+%plot(X(:,1), y, 'k+','LineWidth', 2, 'MarkerSize', 7);
+%feature X2
+%plot(X(:,2), y, 'k+','LineWidth', 2, 'MarkerSize', 7);
 
 end
