1b89a45a462c16a2a754776792cc08c8c7e126b5
José Herazo <joseherazo04@hotmail.com>
2020-07-09 23:18:05 -0500

    implementation of backprop, hard a bit bro

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -61,31 +61,112 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
 % =========================================================================
+
+%Note: I did this test sequentaly, that's way there are some reduntant steps here. But it doesn't matter for me.
+%		Do you understand José of the future? I don't want to clean this code, I don't want now!
+
+%========================PART 1===============================
+% Transform y to vectors
+y_vector = zeros(m, num_labels);
+
+for i = 1:m
+	y_vector(i, y(i)) = 1;
+end
+
+a1 = [ones(m, 1) X];
+
+z2 = a1*Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1) a2];
+
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+h = a3;
+
+%without regularization
+cost = zeros(m,1);
+
+for i = 1:m
+	% Little bit hard to make this operation work, specially because of the sizes of the vectors
+	cost(i) = - y_vector(i,:)*log( h(i,:) )' - (1 - y_vector(i,:))*log( 1 - h(i,:) )';
+	
+	% This way also work
+	%for j = 1:num_labels
+	%	cost(i) = cost(i) + ( - y_vector(i,j) * log( h(i,j) ) - ( 1 - y_vector(i,j) )*log( 1 - h(i,j)));
+	%end
+
+end
+
+% Non-regularized cost function
+J = (1/m)*sum(cost);
+
+%=======Regularization=======
+tempTheta1 = Theta1;
+tempTheta2 = Theta2;
+
+%first element to zero
+tempTheta1(:,1) = 0;
+tempTheta2(:,1) = 0;
+
+%sum them all
+tempTheta1 = sum( sum( tempTheta1.^2 ));
+tempTheta2 = sum( sum( tempTheta2.^2 ));
+ 
+J = J + (lambda/(2*m))*(tempTheta1 + tempTheta2);
+
+
+%========================PART 2===============================
+Delta1 = zeros(size(Theta1));
+Delta2 = zeros(size(Theta2));
+
+for i = 1:m
+														%  X 	matrix 	5000x400
+														%Theta1 matrix  25x401
+														%Theta2 matrix 	10x26
+	%Forward propagation
+	a1 = X( i, :);   									%vector 1x400
+	a1 = a1(:);      									%vector 400x1	Note: there is something reduntant here
+	a1 = [1 ; a1];   									%vector 401x1		  I'll fix it in the future (...Probably)
+	a1 = a1';        									%vector 1x401
+
+	z2 = a1*Theta1'; 									%vector 1x25
+	a2 = sigmoid(z2);									%vector 1x25
+	a2 = [ones(size(a2, 1), 1) a2];						%vector 1x26
+	
+	z3 = a2*Theta2';
+	a3 = sigmoid(z3);									%vector 1x10
+	
+	%Back propagation
+	delta3 = a3 - y_vector( i,:);						%vector 1x10
+	
+	%delta2
+	tempTheta2 = Theta2(:,2:end);						%matrix 10x25
+	sig_grad = sigmoidGradient(z2);						%vector 1x25
+	
+	delta2 = (delta3*tempTheta2).*sig_grad;				%vector 1x25
+	
+	Delta2 = Delta2 + delta3' * a2;						%matrix 10x26
+	Delta1 = Delta1 + delta2' * a1;						%matrix 25x401
+	
+end
+
+Theta1_grad = (1/m).*Delta1;
+Theta2_grad = (1/m).*Delta2;
+
+%========================PART 3===============================
+%Adding regularization
+tempTheta1 = Theta1;
+tempTheta2 = Theta2;
+
+%first element to zero
+tempTheta1(:,1) = 0;
+tempTheta2(:,1) = 0;
+
+Theta1_grad = Theta1_grad + (lambda/m).*tempTheta1;
+Theta2_grad = Theta2_grad + (lambda/m).*tempTheta2;
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
