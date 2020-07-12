f9d64d6fc6b182e75811b5ac0af8fc37f51a4938
José Herazo <joseherazo04@hotmail.com>
2020-07-12 16:58:31 -0500

    processing now done!

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -96,14 +96,15 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
+	
+	%Look if the word str appears in vocabList
+	total = length(vocabList);
+	
+	for i = 1:total
+		if strcmp(str, vocabList{i})
+			word_indices = [word_indices i]; 
+		end
+	end
 
 
     % =============================================================
